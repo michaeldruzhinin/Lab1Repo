e1e3d3d4c6e0afc81bf10a96597840d688eab3f9
michaeldruzhinin <michaeldruzhinin07@gmail.com>
2025-02-23 22:53:21 +0300

    fix: Update main output message in main branch

--- src/main.cpp
+++ src/main.cpp
@@ -7,7 +7,7 @@ int subtract(int a, int b);
 int main() {
     int a = 5, b = 3;
 
-    std::cout << add(a, b) << std::endl;
+    std::cout << add(a, b) << add(b, a) << std::endl;
     std::cout << subtract(a, b) << std::endl;
 
     return 0;
